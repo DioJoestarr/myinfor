af1ed71a42cc4ae344a97babbbb81dbbf4fb1b8d
Lê Đài <daile10a2@gmail.com>
2022-07-17 13:28:52 +0700

    git push

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,21 +1,27 @@
 import React from "react";
-import { motion} from "framer-motion";
+import { animate, motion } from "framer-motion";
+const variants = {
+  spin: {
+    scale: [1, 1.5, 1.5, 1, 1],
+    rotate: [0, 0, 180, 180, 0],
+    borderRadius: ["0%", "0%", "50%", "50%", "50%"],
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+      times: [0, 0.2, 0.5, 0.8, 1],
+      repeat: 0,
+      repeatDelay: 2,
+    },
+  },
+};
 const Animation = () => {
+
   return (
     <motion.div
       className="box"
-      animate={{
-        scale: [1, 2, 2, 1, 1],
-        rotate: [0, 0, 180, 180, 0],
-        borderRadius: ["0%", "0%", "50%", "50%", "50%"],
-      }}
-      transition={{
-        duration: 2,
-        ease: "easeInOut",
-        times: [0, 0.2, 0.5, 0.8, 1],
-        repeat: 0,
-        repeatDelay: 1,
-      }}
+      animate="spin"
+      whileHover={"spin"}
+      variants={variants}
     />
   );
 };
