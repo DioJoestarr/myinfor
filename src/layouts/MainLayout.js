8535582351995db2ce25dfcf0ddceec810a4c71d
Lê Đài <92147870+DioJoestarr@users.noreply.github.com>
2022-08-09 11:48:36 +0700

    Update MainLayout.js

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -13,14 +13,8 @@ const MainLayout = () => {
           />
         </div>
         <div className="flex flex-col items-center w-full justify-center relative top-[-96px]">
-          
-             <img
-              className="rounded-full w-full h-fit border-4 border-black"
-              src="https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-6/250748865_2861777274113291_1110232546663015312_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=EvqLk7_bsrcAX82qSW-&_nc_ht=scontent.fsgn2-6.fna&oh=00_AT-Oaq1clDFA0Li29tXBIfX-f0pRj5nhLpa1PPzbqrLyPA&oe=62D70168"
-              alt=""
-            /> 
             <Animation />
-          
+         
           <div className="w-full p-2">
             <div className="name">
               <span className="text-white font-sans font-medium text-center text-5xl md:text-6xl">
